a048bd4844fb87addf2347ddc196dc859f453cc9
Monika <Monika.cyberflax@gmail.com>
2022-11-14 19:53:43 +0530

    change

--- about/views.py
+++ about/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from category.models import category_types
+
 # Create your views here.
 def about(request):
-    return render(request, 'about.html')
+    cat = category_types.objects.all()
+    context = {'cat':cat}
+    return render(request, 'about.html', context)
